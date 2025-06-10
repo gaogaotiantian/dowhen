b0949c5377933790349b7a12df15e0cdb5c92bc9
Tian Gao <gaogaotiantian@hotmail.com>
2025-06-10 13:56:25 -0700

    Add type hints to all functions

--- src/dowhen/instrumenter.py
+++ src/dowhen/instrumenter.py
@@ -44,6 +44,9 @@ class Instrumenter:
     def submit(self, event_handler: EventHandler):
         event = event_handler.event
         if event.event_type == "line":
+            assert (
+                isinstance(event.event_data, dict) and "line_number" in event.event_data
+            )
             self.register_line_event(
                 event.code,
                 event.event_data["line_number"],
@@ -106,6 +109,9 @@ class Instrumenter:
         ):
             return
         if event.event_type == "line":
+            assert (
+                isinstance(event.event_data, dict) and "line_number" in event.event_data
+            )
             handlers = self.handlers[event.code]["line"].get(
                 event.event_data["line_number"], []
             )
@@ -116,6 +122,10 @@ class Instrumenter:
             handlers.remove(event_handler)
 
             if event.event_type == "line" and not handlers:
+                assert (
+                    isinstance(event.event_data, dict)
+                    and "line_number" in event.event_data
+                )
                 del self.handlers[event.code]["line"][event.event_data["line_number"]]
 
             if not self.handlers[event.code][event.event_type]:
